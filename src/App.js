5a5716990001f04c1c76fe1258eaa0ffb94fd729
Shiva Venkatesh <shiva@bangthetable.com>
2017-10-22 11:43:08 +0530

    package: add semantic dropdown package for filters

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import axios from 'axios'
 
 import Issue from './components/issue.js'
+import Dropdown from './components/dropdown.js'
 
 import './App.css'
 
@@ -9,7 +10,8 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      issues: []
+      issues: [],
+      labels: []
     }
   }
 
@@ -17,6 +19,14 @@ class App extends Component {
     axios.get('https://api.github.com/repos/zeit/next.js/issues?page=7')
     .then((response) => {
       this.setState({issues: response.data})
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+    axios.get('https://api.github.com/repos/zeit/next.js/labels')
+    .then((response) => {
+      this.setState({labels: response.data})
     })
     .catch(function (error) {
       console.log(error);
@@ -35,7 +45,9 @@ class App extends Component {
         <div className="container app-body">
           <h3 className="repo-name col-sm-12">Current Repository : Next.js</h3>
           <div className="col-sm-12 filters">
-
+          <div className="col-sm-3">
+            <Dropdown />
+          </div>
           </div>
           <div className="issues col-sm-12">
             {renderIssue}
