63ab2946fdc530e35a739008fe2e701dc48ba290
Shiva Venkatesh <shiva@bangthetable.com>
2017-10-22 14:42:24 +0530

    data: populate filters with data from API call

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import _ from 'lodash'
 
 import Issue from './components/issue.js'
-import Dropdown from './components/dropdown.js'
+import FilterDropdown from './components/dropdown.js'
 
 import './App.css'
 
@@ -11,11 +12,18 @@ class App extends Component {
     super(props)
     this.state = {
       issues: [],
-      labels: []
+      fetchedLabels: [],
+      formattedLabels: []
     }
   }
 
   componentWillMount() {
+    let dropdownOptionObject = {
+      name : "text",
+      id : "key",
+      color: "value"
+    }
+
     axios.get('https://api.github.com/repos/zeit/next.js/issues?page=7')
     .then((response) => {
       this.setState({issues: response.data})
@@ -26,11 +34,25 @@ class App extends Component {
 
     axios.get('https://api.github.com/repos/zeit/next.js/labels')
     .then((response) => {
-      this.setState({labels: response.data})
+      this.setState({fetchedLabels: response.data}, () => this.mapLabels(dropdownOptionObject))
     })
     .catch(function (error) {
       console.log(error);
     });
+  }
+
+  mapLabels(dropdownOptionObject) {
+    let newLabelObject = {}
+    let formattedLabels = []
+    this.state.fetchedLabels.map((label, index) => {
+      return(
+         newLabelObject = _.mapKeys(label, function(value, key) {
+            return dropdownOptionObject[key];
+          }),
+         formattedLabels[index] = newLabelObject
+        )
+    })
+    this.setState({formattedLabels: formattedLabels})
   }
 
   render() {
@@ -45,8 +67,8 @@ class App extends Component {
         <div className="container app-body">
           <h3 className="repo-name col-sm-12">Current Repository : Next.js</h3>
           <div className="col-sm-12 filters">
-          <div className="col-sm-3">
-            <Dropdown />
+          <div className="col-sm-4 filter-box">
+            <FilterDropdown placeholder={'Labels'} labelOptions={this.state.formattedLabels} />
           </div>
           </div>
           <div className="issues col-sm-12">
